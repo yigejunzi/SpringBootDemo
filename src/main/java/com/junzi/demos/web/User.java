a5a36e5037a25e5a10f6b8bb38eb10d03bf16a76
yigejunzi <1814062269@qq.com>
2024-10-25 12:05:21 +0800

    添加地址属性

--- src/main/java/com/junzi/demos/web/User.java
+++ src/main/java/com/junzi/demos/web/User.java
@@ -24,6 +24,15 @@ public class User {
     private String name;
 
     private Integer age;
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     public String getName() {
         return name;
